6143fc860c6b54eb98e20f142717c21f454f72f6
2018wsbTest <rafalsobieszczyk@gmail.com>
2019-12-15 03:05:15 +0100

    tests

---
+++ task.py
@@ -0,0 +1,69 @@
+""""
+Rozwiąż zadanie
+Wykorzystując paradygmaty programowania obiektowego napisz program który będzie pozwalał na obliczanie pół
+i objętości figur płaskich oraz wielościanów.
+* trójkąt* triangle                         1/2*a*h
+* kwadrat* square                           a*a
+* prostokąt* rectangle                      a*b
+* sześcian* cube                            a*a*6       a*a*a
+* ostrosłup foremny* regular pyramid
+    -   * trójkąt* triangle     tetrahedron                    1/2*a*h
+    -   * kwadrat* square                           a*a
+    -
+* owal* oval                                P=πab
+"""
+import math
+
+
+class SurfaceArea():
+
+    def __init__(self, basis):
+        self.basis = basis
+        self.height = 0
+        self.side = 0
+
+    def triangle_surface_area(self, height):
+        self.height = height
+        return 0.5*self.basis*self.height
+
+    def aquare_sourface_area(self):
+        return self.basis**2
+
+    def rectangle_sourface_area(self, side):
+        self.side = side
+        return self.side*self.basis
+
+    def cube_sourface_area(self):
+        return (self.basis**2)*6
+
+    def oval_sourface_area(self, side):
+        self.side = side
+        return math.pi*self.side*self.basis
+
+    def tetrahedron_sourface_area(self):
+        return (self.basis**2)*math.sqrt(3)
+
+
+class Volume(SurfaceArea):
+    def __init__(self, basis):
+        super().__init__(basis)
+
+    def cube_volume(self):
+        return self.basis**3
+
+    def tetrahedron_volume(self):
+        return ((self.basis**3)*math.sqrt(2))/12
+
+
+
+area = SurfaceArea(3)
+print(f"pole trójkąta: {area.triangle_surface_area(4):.2f}")
+print(f"pole kwadratu: {area.aquare_sourface_area():.2f}")
+print(f"pole prostokąta: {area.rectangle_sourface_area(12):.2f}")
+print(f"pole powierzchni ścian sześcianu: {area.cube_sourface_area():.2f}")
+print(f"Pole powierzchni owalu: {area.oval_sourface_area(10):.2f}")
+print(f"pole powierzchni ścian czworościanu foremnego: {area.tetrahedron_sourface_area():.2f}")
+
+volume = Volume(4)
+print(f"objętość sześcianu: {volume.cube_volume():.2f}")
+print(f"objętość czworościanu foremnego: {volume.tetrahedron_volume():.2f}")
---
+++ test_Surface_area.py
@@ -0,0 +1,42 @@
+import unittest
+import task
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_triangle_surface_area(self):
+        area = task.SurfaceArea(3).triangle_surface_area(4)
+        self.assertEquals(area, 6)
+
+    def test_aquare_sourface_area(self):
+        area = task.SurfaceArea(4).aquare_sourface_area()
+        self.assertEquals(area, 16)
+
+    def test_rectangle_sourface_area(self):
+        area = task.SurfaceArea(5).rectangle_sourface_area(3)
+        self.assertEquals(area, 15)
+
+    def test_cube_sourface_area(self):
+        area = task.SurfaceArea(5).cube_sourface_area()
+        self.assertEquals(area, 150)
+
+    def test_oval_sourface_area(self):
+        area = task.SurfaceArea(3).oval_sourface_area(10)
+        self.assertEquals(area, 94.24777960769379)
+
+    def test_tetrahedron_sourface_area(self):
+        area = task.SurfaceArea(3).tetrahedron_sourface_area()
+        self.assertEquals(area, 15.588457268119894)
+
+    def test_cube_volume(self):
+        volume = task.Volume(4).cube_volume()
+        self.assertEquals(volume, 64)
+
+    def test_tetrahedron_volume(self):
+        volume = task.Volume(4).tetrahedron_volume()
+        self.assertEquals(volume, 7.542472332656508)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
